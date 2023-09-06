d518ba6b0b02085a8c8fd0c84a41ca9b3eccabc3
yooyouny <jhjin2u@hanmail.com>
2023-09-06 22:29:26 +0900

    initial commit

---
+++ src/main/java/com/devlop/siren/SirenApplication.java
@@ -0,0 +1,13 @@
+package com.devlop.siren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SirenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SirenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devlop/siren/SirenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlop.siren;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SirenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
