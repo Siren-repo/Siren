bd4acc5b4f436e0d2b38932212c525e564c5bf4b
yoonjuho <dbswn990@naver.com>
2023-09-20 04:38:23 +0900

    feat: 아이템 디폴트 옵션 구현 (#9)
    - 아이템의 디폴트 옵션 정보를 가지고있는 객체
    - 바닐라시럽 샷이나 에스프레소 샷 등의 값이 사용자로부터 들어오지 않았을때 persist 전에 default값으로 설정해주는 prePersist 메서드
    - 디폴트 옵션의 정보를 업데이트 할 수 있는 update메서드

---
+++ src/main/java/com/devlop/siren/item/domain/DefaultOption.java
@@ -0,0 +1,63 @@
+package com.devlop.siren.item.domain;
+
+import java.util.Objects;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import com.devlop.siren.item.dto.DefaultOptionCreateRequest;
+import lombok.*;
+
+@Table(name = "default_options")
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class DefaultOption {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long defaultOptionId;
+
+    @Column(name = "espresso_shot_count", columnDefinition = "TINYINT")
+    private Integer espressoShotCount;
+
+    @Column(name = "vanilla_syrup_count", columnDefinition = "TINYINT")
+    private Integer vanillaSyrupCount;
+
+    @Column(name = "caramel_syrup_count", columnDefinition = "TINYINT")
+    private Integer caramelSyrupCount;
+
+    @Column(name = "hazelnut_syrup_count", columnDefinition = "TINYINT")
+    private Integer hazelnutSyrupCount;
+
+    @Column(name = "size")
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private SizeType size;
+
+    @Builder
+    public DefaultOption(Integer espressoShotCount, Integer vanillaSyrupCount, Integer caramelSyrupCount, Integer hazelnutSyrupCount, SizeType size) {
+        this.espressoShotCount = espressoShotCount;
+        this.vanillaSyrupCount = vanillaSyrupCount;
+        this.caramelSyrupCount = caramelSyrupCount;
+        this.hazelnutSyrupCount = hazelnutSyrupCount;
+        this.size = size;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        this.espressoShotCount = this.espressoShotCount == null ? 0 : this.espressoShotCount;
+        this.vanillaSyrupCount = this.vanillaSyrupCount == null ? 0 : this.vanillaSyrupCount;
+        this.caramelSyrupCount = this.caramelSyrupCount == null ? 0 : this.vanillaSyrupCount;
+        this.hazelnutSyrupCount = this.hazelnutSyrupCount == null ? 0 : this.hazelnutSyrupCount;
+    }
+
+    public void update(DefaultOptionCreateRequest defaultOptionCreateRequest){
+        this.espressoShotCount = defaultOptionCreateRequest.getEspressoShotCount();
+        this.vanillaSyrupCount = defaultOptionCreateRequest.getVanillaSyrupCount();
+        this.caramelSyrupCount = defaultOptionCreateRequest.getCaramelSyrupCount();
+        this.hazelnutSyrupCount = defaultOptionCreateRequest.getHazelnutSyrupCount();
+        this.size = defaultOptionCreateRequest.getSize();
+    }
+
+}
