0eaf11fac3e8c6d983fe962aac9bd21f7e782836
yooyouny <jhjin2u@hanmail.com>
2023-10-01 02:09:08 +0900

    feat: ControllerAdvice 구현

--- src/main/java/com/devlop/siren/global/exception/ExceptionControllerAdvice.java
+++ src/main/java/com/devlop/siren/global/exception/ExceptionControllerAdvice.java
@@ -1,2 +1,38 @@
-package com.devlop.siren.global.exception;public class ExceptionControllerAdvice {
+package com.devlop.siren.global.exception;
+
+import com.devlop.siren.global.common.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+@Slf4j
+public class ExceptionControllerAdvice {
+
+    @ExceptionHandler(GlobalException.class)
+    public ResponseEntity<?> globalException(final GlobalException exception) {
+        log.error("Error occurs {}", exception.toString());
+
+        return ResponseEntity.status(exception.getErrorCode().getStatus())
+                .body(ApiResponse.error(exception.getErrorCode()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> validException(final MethodArgumentNotValidException exception) {
+        log.error("Error occurs {} in MethodArgumentNotValidException", exception.toString());
+
+        List<String> errorMessages = exception.getBindingResult().getAllErrors().stream()
+                .map(error -> error.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error(HttpStatus.BAD_REQUEST, HttpStatus.BAD_REQUEST.name(),errorMessages));
+    }
+
 }
--- src/main/java/com/devlop/siren/global/exception/GlobalException.java
+++ src/main/java/com/devlop/siren/global/exception/GlobalException.java
@@ -1,2 +1,14 @@
-package com.devlop.siren.global.exception;public class GlobalException {
+package com.devlop.siren.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public class GlobalException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public GlobalException(ErrorCode errorCode) {
+        super(errorCode.getMESSAGE());
+        this.errorCode = errorCode;
+    }
 }
