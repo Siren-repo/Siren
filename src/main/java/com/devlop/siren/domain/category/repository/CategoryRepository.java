cbdf42d3cfedcf3c6efdc1c12d94a995d8434c1f
yoonjuho <dbswn990@naver.com>
2023-10-02 01:57:33 +0900

    feat: 카테고리 리파지토리 구현 (#9)

---
+++ src/main/java/com/devlop/siren/domain/category/repository/CategoryRepository.java
@@ -0,0 +1,17 @@
+package com.devlop.siren.domain.category.repository;
+
+import com.devlop.siren.domain.category.entity.Category;
+import com.devlop.siren.domain.category.entity.CategoryType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByCategoryTypeAndCategoryName(@Param("categoryType") CategoryType categoryType,
+                                                         @Param("categoryName") String categoryName);
+
+    Optional<List<Category>> findByCategoryTypeOrderByCategoryId(@Param("categoryType") CategoryType categoryType);
+
+}
