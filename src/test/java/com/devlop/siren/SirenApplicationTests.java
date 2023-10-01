43bc9642e37a8c2e1ce6ab0ad4c5034319173f2c
yoonjuho <dbswn990@naver.com>
2023-10-02 02:02:11 +0900

    test: 아이템 컨트롤러/서비스 단위 테스트 (#9)

--- src/test/java/com/devlop/siren/SirenApplicationTests.java
+++ src/test/java/com/devlop/siren/SirenApplicationTests.java
@@ -1,13 +1,12 @@
 package com.devlop.siren;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SirenApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
