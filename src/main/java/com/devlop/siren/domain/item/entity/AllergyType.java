831953a481dbd38b097afa7568cfe5aa60080d83
yoonjuho <dbswn990@naver.com>
2023-10-02 01:51:12 +0900

    refactor: 알러지 정보 구현 (#9)

---
+++ src/main/java/com/devlop/siren/domain/item/entity/AllergyType.java
@@ -0,0 +1,21 @@
+package com.devlop.siren.domain.item.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum AllergyType {
+    MILK("우유"),
+    SOYBEAN("대두"),
+    WHEAT("밀"),
+    PEANUT("땅콩"),
+    TOMATO("토마토"),
+    PEACH("복숭아"),
+    SQUID("오징어");
+
+    private final String allergyName;
+
+    AllergyType(String allergyName) {
+        this.allergyName = allergyName;
+    }
+
+}
