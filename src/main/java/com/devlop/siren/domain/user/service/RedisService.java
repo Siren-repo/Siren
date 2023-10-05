2f81ccedb8631b92b9c1b484517333b16f7fd3d7
yooyouny <jhjin2u@hanmail.com>
2023-10-05 15:55:45 +0900

    feat: redis 관련 설정 및 템플릿 구현 (#14)

--- src/main/java/com/devlop/siren/domain/user/service/RedisService.java
+++ src/main/java/com/devlop/siren/domain/user/service/RedisService.java
@@ -1,2 +1,30 @@
-package com.devlop.siren.domain.user.service;public class RedisService {
+package com.devlop.siren.domain.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+@Component
+@RequiredArgsConstructor
+public class RedisService {
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public void setValue(String key, String value){
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        values.set(key, value);
+    }
+    public void setValue(String key, String value, Duration duration){
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        values.set(key, value, duration);
+    }
+    public String getValues(String key){
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        return values.get(key);
+    }
+    public void deleteValue(String key){
+        redisTemplate.delete(key);
+    }
 }
