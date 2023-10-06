5eac3e85faf84b529d4ddec52432d6d02b912212
yoonjuho <dbswn990@naver.com>
2023-10-06 17:21:35 +0900

    refactor: NotValid 예외처리 방식 변경 (#9)

--- src/main/java/com/devlop/siren/global/exception/ExceptionControllerAdvice.java
+++ src/main/java/com/devlop/siren/global/exception/ExceptionControllerAdvice.java
@@ -2,6 +2,7 @@ package com.devlop.siren.global.exception;
 
 import com.devlop.siren.global.common.response.ApiResponse;
 import com.devlop.siren.global.common.response.ResponseCode;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -19,16 +20,15 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import javax.validation.ConstraintViolationException;
 
 @RestControllerAdvice
-public class ExceptionControllerAdvice extends ResponseEntityExceptionHandler {
-    private final Logger logger = LoggerFactory.getLogger(ExceptionControllerAdvice.class);
-
+@Slf4j
+public class ExceptionControllerAdvice {
 
     //@Valid 검증 실패 시 Catch
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        BindingResult result = ex.getBindingResult();
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<?> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        BindingResult result = e.getBindingResult();
         StringBuilder errMessage = new StringBuilder();
-        logger.info("errMsg ### handleMethodArgumentNotValid");
+        log.error("Error occurs {}", e.toString());
         for (FieldError error : result.getFieldErrors()) {
             errMessage.append("[")
                     .append(error.getField())
@@ -36,16 +36,15 @@ public class ExceptionControllerAdvice extends ResponseEntityExceptionHandler {
                     .append(":")
                     .append(error.getDefaultMessage());
         }
-
-        return ResponseEntity.status(ResponseCode.ErrorCode.NOT_VALID.getStatus()).body(ApiResponse.error(ResponseCode.ErrorCode.NOT_VALID.getStatus(), ex.getMessage()));
-
+        return ResponseEntity.status(ResponseCode.ErrorCode.NOT_VALID.getStatus()).body(ApiResponse.error(ResponseCode.ErrorCode.NOT_VALID.getStatus(), e.getMessage()));
     }
+
 
 
     //@Valid 검증 실패 시 Catch - Get
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ApiResponse<?>> handleConstraintViolationException(ConstraintViolationException e) {
-        logger.info("errMsg ### handleConstraintViolationException");
+        log.error("Error occurs {}", e.toString());
         return ResponseEntity.status(ResponseCode.ErrorCode.NOT_VALID.getStatus()).body(ApiResponse.error(ResponseCode.ErrorCode.NOT_VALID.getStatus(), e.getMessage()));
 
     }
@@ -53,14 +52,13 @@ public class ExceptionControllerAdvice extends ResponseEntityExceptionHandler {
     //DeletedById에서 내부적으로 생기는 예외처리
     @ExceptionHandler(EmptyResultDataAccessException.class)
     public ResponseEntity<ApiResponse<?>> handleEmptyResultDataAccessException(EmptyResultDataAccessException e) {
-        logger.info("errMsg ### handleEmptyResultDataAccessException");
+        log.error("Error occurs {}", e.toString());
         return ResponseEntity.status(ResponseCode.ErrorCode.NOT_FOUND_ITEM.getStatus()).body(ApiResponse.error(ResponseCode.ErrorCode.NOT_FOUND_ITEM.getStatus(), e.getMessage()));
     }
 
     @ExceptionHandler(GlobalException.class)
     public ResponseEntity<?> globalException(final GlobalException exception) {
-        logger.error("Error occurs {}", exception.toString());
-
+        log.error("Error occurs {}", exception.toString());
         return ResponseEntity.status(exception.getErrorCode().getStatus())
                 .body(ApiResponse.error(exception.getErrorCode()));
     }
