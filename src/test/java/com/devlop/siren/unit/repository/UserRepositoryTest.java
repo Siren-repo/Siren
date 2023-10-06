9636d26cf9dff34e661895b8231fe28ae75a1fe4
yooyouny <jhjin2u@hanmail.com>
2023-10-06 13:38:55 +0900

    chore: repository test의 프로파일 변경 및 h2 db url 변경

--- src/test/java/com/devlop/siren/unit/repository/UserRepositoryTest.java
+++ src/test/java/com/devlop/siren/unit/repository/UserRepositoryTest.java
@@ -10,12 +10,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.EnumSet;
 
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 class UserRepositoryTest {
 
     @Autowired
