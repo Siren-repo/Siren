7c527ae582fd663f4b26353abdde07e24e033c88
yooyouny <jhjin2u@hanmail.com>
2023-10-12 21:29:20 +0900

    chore: valid exception 응답코드 400으로 통일

--- src/main/java/com/devlop/siren/global/exception/ExceptionControllerAdvice.java
+++ src/main/java/com/devlop/siren/global/exception/ExceptionControllerAdvice.java
@@ -1,45 +1,68 @@
 package com.devlop.siren.global.exception;
 
-import com.devlop.siren.global.common.ApiResponse;
+import com.devlop.siren.global.common.response.ApiResponse;
+import com.devlop.siren.global.common.response.ResponseCode;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.validation.ConstraintViolationException;
 
 @RestControllerAdvice
 @Slf4j
 public class ExceptionControllerAdvice {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<?> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        BindingResult result = e.getBindingResult();
+        StringBuilder errMessage = new StringBuilder();
+        log.error("Error occurs {}", e.toString());
+        for (FieldError error : result.getFieldErrors()) {
+            errMessage.append("[")
+                    .append(error.getField())
+                    .append("] ")
+                    .append(":")
+                    .append(error.getDefaultMessage());
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error(HttpStatus.BAD_REQUEST, e.getMessage()));
+    }
+
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ApiResponse<?>> handleConstraintViolationException(ConstraintViolationException e) {
+        log.error("Error occurs {}", e.toString());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error(HttpStatus.BAD_REQUEST, e.getMessage()));
+
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<ApiResponse<?>> handleEmptyResultDataAccessException(EmptyResultDataAccessException e) {
+        log.error("Error occurs {}", e.toString());
+        return ResponseEntity.status(ResponseCode.ErrorCode.NOT_FOUND_ITEM.getStatus())
+                .body(ApiResponse.error(ResponseCode.ErrorCode.NOT_FOUND_ITEM.getStatus(), e.getMessage()));
+    }
 
     @ExceptionHandler(GlobalException.class)
     public ResponseEntity<?> globalException(final GlobalException exception) {
         log.error("Error occurs {}", exception.toString());
-
         return ResponseEntity.status(exception.getErrorCode().getStatus())
                 .body(ApiResponse.error(exception.getErrorCode()));
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> validException(final MethodArgumentNotValidException exception) {
-        log.error("Error occurs {} in MethodArgumentNotValidException", exception.toString());
-
-        List<String> errorMessages = exception.getBindingResult().getAllErrors().stream()
-                .map(error -> error.getDefaultMessage())
-                .collect(Collectors.toList());
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(ApiResponse.error(HttpStatus.BAD_REQUEST, HttpStatus.BAD_REQUEST.name(),errorMessages));
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<?> applicationHandler(RuntimeException e){
         log.error("Error occurs {}", e.toString());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(ApiResponse.error(HttpStatus.INTERNAL_SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR.name(), null));
+                .body(ApiResponse.error(ResponseCode.ErrorCode.INTERNAL_SERVER_ERROR));
     }
 
 }
