d1e4e202932cd46133e4e31b767d8d68b72ef0e4
yoonjuho <dbswn990@naver.com>
2023-10-20 20:50:51 +0900

    feat: 사용자 인증,검증 클래스 구현(#19)

---
+++ src/main/java/com/devlop/siren/global/util/UserInformation.java
@@ -0,0 +1,16 @@
+package com.devlop.siren.global.util;
+
+
+import com.devlop.siren.domain.user.domain.UserRole;
+import com.devlop.siren.domain.user.dto.UserDetailsDto;
+import com.devlop.siren.global.common.response.ResponseCode;
+import com.devlop.siren.global.exception.GlobalException;
+
+public class UserInformation {
+    public static boolean validAdmin(UserDetailsDto user) {
+        if (!user.getUserRole().equals(UserRole.ADMIN)) {
+            throw new GlobalException(ResponseCode.ErrorCode.NOT_AUTHORITY_USER);
+        }
+        return true;
+    }
+}
