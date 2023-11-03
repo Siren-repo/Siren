3a5aabcb2d4109110b44040439687468a4f053e2
yoonjuho <dbswn990@naver.com>
2023-11-03 12:09:48 +0900

    feat: 유니크 제약조건 추가(#19)

--- src/main/java/com/devlop/siren/domain/stock/entity/Stock.java
+++ src/main/java/com/devlop/siren/domain/stock/entity/Stock.java
@@ -2,24 +2,19 @@ package com.devlop.siren.domain.stock.entity;
 
 import com.devlop.siren.domain.item.entity.Item;
 import com.devlop.siren.domain.store.domain.Store;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "stocks")
 @Getter
+@Table(name = "stocks", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"item_id", "store_id"})
+})
 public class Stock {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
