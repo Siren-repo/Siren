d41d6f9e0e55d3ecf7d7efcdd55702c361aaae7c
yoonjuho <dbswn990@naver.com>
2023-11-03 14:24:58 +0900

    refactor: 반환형 페이징 처리(#19)

--- src/main/java/com/devlop/siren/domain/stock/dto/response/StocksResponse.java
+++ src/main/java/com/devlop/siren/domain/stock/dto/response/StocksResponse.java
@@ -1,11 +1,14 @@
 package com.devlop.siren.domain.stock.dto.response;
 
 import java.util.List;
+
+import com.devlop.siren.global.common.response.PageInfo;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class StocksResponse {
-    private List<StockResponse> stocks;
+public class StocksResponse<T> {
+    private T stocks;
+    private PageInfo pageInfo;
 }
