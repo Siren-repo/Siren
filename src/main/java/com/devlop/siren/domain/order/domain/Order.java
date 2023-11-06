906a4620d95f53992e53aec2d07e97593112ae4d
yooyouny <jhjin2u@hanmail.com>
2023-11-06 18:52:15 +0900

    chore: 기본키 컬럼명을 엔티티이름_id로 변경 (#23)

--- src/main/java/com/devlop/siren/domain/order/domain/Order.java
+++ src/main/java/com/devlop/siren/domain/order/domain/Order.java
@@ -18,6 +18,7 @@ import java.util.List;
 public class Order extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
--- src/main/java/com/devlop/siren/domain/user/domain/User.java
+++ src/main/java/com/devlop/siren/domain/user/domain/User.java
@@ -29,6 +29,7 @@ public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
 
     @Column(unique = true)
