b56f4d552d118c627b71af44adc80b3d4d01c444
yoonjuho <dbswn990@naver.com>
2023-11-10 13:59:04 +0900

    refactor: DTO 반환을 위한 json형식 추가 (#32)

--- src/main/java/com/devlop/siren/domain/item/entity/option/OptionTypeGroup.java
+++ src/main/java/com/devlop/siren/domain/item/entity/option/OptionTypeGroup.java
@@ -1,26 +1,49 @@
 package com.devlop.siren.domain.item.entity.option;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public class OptionTypeGroup {
 
-    public enum Temperature{
+    public enum Temperature {
         HOT, COLD, NONE
     }
-    public enum Amount{
+
+    public enum Amount {
         LITTLE, NORMAL, MUCH, NONE;
     }
-    public enum EspressoType{
-        ORIGINAL, DECAFFEINE
+
+    public enum EspressoType {
+        ORIGINAL, DECAFFEINE;
+
+        @JsonCreator
+        public EspressoType fromString(String value) {
+            return EspressoType.valueOf(value.toUpperCase());
+        }
     }
-    public enum MilkType{
-        ORIGINAL, LOW_FAT, FAT_FREE, SOY, OAT
+
+    public enum MilkType {
+        ORIGINAL, LOW_FAT, FAT_FREE, SOY, OAT;
+
+        @JsonCreator
+        public MilkType fromString(String value) {
+            return MilkType.valueOf(value.toUpperCase());
+        }
     }
-    public enum FoamType{
+
+    public enum FoamType {
         MILK, ESPRESSO, MATCHA
     }
-    public enum SyrupType{
+
+    public enum SyrupType {
         VANILLA, CARAMEL, HAZELNUT, CLASSIC;
+
+        @JsonCreator
+        public SyrupType fromString(String value) {
+            return SyrupType.valueOf(value.toUpperCase());
+        }
     }
-    public enum DrizzleType{
+
+    public enum DrizzleType {
         CHOCOLATE, CARAMEL;
     }
 }
