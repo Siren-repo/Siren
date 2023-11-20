6a75dc10feca427fbbccf63198a62c114b21a1a3
yoonjuho <dbswn990@naver.com>
2023-11-20 11:15:12 +0900

    refactor: 권한 처리 담당 layer 변경 (#32)

--- src/main/java/com/devlop/siren/domain/category/dto/request/CategoryCreateRequest.java
+++ src/main/java/com/devlop/siren/domain/category/dto/request/CategoryCreateRequest.java
@@ -1,6 +1,7 @@
 package com.devlop.siren.domain.category.dto.request;
 
 
+import com.devlop.siren.domain.category.entity.Category;
 import com.devlop.siren.domain.category.entity.CategoryType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,5 +19,11 @@ public class CategoryCreateRequest {
 
     @NotBlank(message = "아이템 카테고리가 입력되지 않았습니다.")
     private String categoryName;
+
+    public static Category toEntity(CategoryCreateRequest request){
+        return Category.builder()
+                .categoryName(request.getCategoryName())
+                .categoryType(request.getCategoryType()).build();
+    }
 
 }
--- src/test/java/com/devlop/siren/category/service/CategoryServiceTest.java
+++ src/test/java/com/devlop/siren/category/service/CategoryServiceTest.java
@@ -5,8 +5,13 @@ import com.devlop.siren.domain.category.entity.Category;
 import com.devlop.siren.domain.category.entity.CategoryType;
 import com.devlop.siren.domain.category.repository.CategoryRepository;
 import com.devlop.siren.domain.category.service.CategoryService;
+import com.devlop.siren.domain.user.domain.UserRole;
+import com.devlop.siren.domain.user.dto.UserDetailsDto;
+import com.devlop.siren.global.common.response.ResponseCode;
 import com.devlop.siren.global.exception.GlobalException;
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,6 +22,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -30,25 +36,41 @@ class CategoryServiceTest {
     @InjectMocks
     private CategoryService categoryService;
 
-    static private CategoryCreateRequest validObject;
+    private CategoryCreateRequest validObject;
+    private UserDetailsDto staff;
+    private UserDetailsDto customer;
 
-    @BeforeAll
-    private static void setUp() {
+    @BeforeEach
+    private void setUp() {
         validObject = new CategoryCreateRequest(CategoryType.of("음료"), "에스프레소");
+        staff = new UserDetailsDto(1L, "test@test.com", "12345678", UserRole.ADMIN, false);
+        customer = new UserDetailsDto(2L, "test1@test.com", "12345678", UserRole.CUSTOMER, false);
     }
 
     @Test
-    @DisplayName("카테고리를 생성할 수 있다")
+    @DisplayName("권한이 있는 사용자는 카테고리를 생성할 수 있다")
     public void register() {
         //given
-        Long categoryId = 1L;
-        Category category = Category.builder().categoryId(categoryId).categoryType(validObject.getCategoryType()).categoryName(validObject.getCategoryName()).build();
+        Category category = CategoryCreateRequest.toEntity(validObject);
         //when
         when(categoryRepository.findByCategoryTypeAndCategoryName(any(), any())).thenReturn(Optional.empty());
         when(categoryRepository.save(any(Category.class))).thenReturn(category);
         //then
-        assertThat(categoryService.register(validObject).getCategoryId()).isEqualTo(categoryId);
+        assertThat(categoryService.register(validObject, staff).getCategoryName()).isEqualTo(validObject.getCategoryName());
     }
+
+    @Test
+    @DisplayName("권한이 없는 사용자는 카테고리를 생성할 수 없다")
+    public void failRegister() {
+        //given
+        Throwable throwable = catchThrowable(() -> categoryService.register(validObject, customer));
+        // Then
+        AssertionsForClassTypes.assertThat(throwable)
+                .isInstanceOf(GlobalException.class)
+                .hasMessageContaining(ResponseCode.ErrorCode.NOT_AUTHORITY_USER.getMESSAGE());
+    }
+
+
 
     @Test
     @DisplayName("이미 생성된 카테고리를 중복생성할 수 없다")
@@ -59,7 +81,7 @@ class CategoryServiceTest {
         //when
         when(categoryRepository.findByCategoryTypeAndCategoryName(any(), any())).thenReturn(Optional.ofNullable(category));
         //then
-        assertThrows(GlobalException.class, () -> categoryService.register(validObject));
+        assertThrows(GlobalException.class, () -> categoryService.register(validObject, staff));
     }
 
 }
