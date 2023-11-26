ddcfdfd2fef41cc9d6466acb1050362574c644ea
yooyouny <jhjin2u@hanmail.com>
2023-11-27 01:31:02 +0900

    feat: OptionType에 Json변환 추가 (#23) - String을 enum객체로 변환하는 메소드를 enum 타입들에 모두 추가

--- src/main/java/com/devlop/siren/domain/item/entity/option/OptionDetails.java
+++ src/main/java/com/devlop/siren/domain/item/entity/option/OptionDetails.java
@@ -1,109 +1,103 @@
 package com.devlop.siren.domain.item.entity.option;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
+import com.devlop.siren.domain.item.entity.option.OptionTypeGroup.Amount;
+import com.devlop.siren.domain.item.entity.option.OptionTypeGroup.DrizzleType;
+import com.devlop.siren.domain.item.entity.option.OptionTypeGroup.EspressoType;
+import com.devlop.siren.domain.item.entity.option.OptionTypeGroup.FoamType;
+import com.devlop.siren.domain.item.entity.option.OptionTypeGroup.SyrupType;
 import javax.persistence.Embeddable;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-
-import com.devlop.siren.domain.item.entity.option.OptionTypeGroup.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 public class OptionDetails {
-    @Embeddable
-    @Getter
-    @NoArgsConstructor
-    public class SyrupDetail {
-        @Enumerated(EnumType.STRING)
-        private SyrupType syrupType;
+  @Embeddable
+  @Getter
+  @NoArgsConstructor
+  public static class SyrupDetail {
+    @Enumerated(EnumType.STRING)
+    private SyrupType syrupType;
 
-        private Integer cnt;
-        public SyrupDetail(SyrupType type, int cnt){
-            this.syrupType = type;
-            this.cnt = cnt;
-        }
+    private Integer cnt;
+
+    public SyrupDetail(SyrupType type, int cnt) {
+      this.syrupType = type;
+      this.cnt = cnt;
     }
-    @Embeddable
-    @Getter
-    @NoArgsConstructor
-    public static class DrizzleDetail {
-        @Enumerated(EnumType.STRING)
-        private DrizzleType drizzleType;
+  }
 
-        private Integer cnt;
+  @Embeddable
+  @Getter
+  @NoArgsConstructor
+  public static class DrizzleDetail {
+    @Enumerated(EnumType.STRING)
+    private DrizzleType drizzleType;
 
-        public DrizzleDetail(DrizzleType type, int cnt){
-            this.drizzleType = type;
-            this.cnt = cnt;
-        }
+    private Integer cnt;
+
+    public DrizzleDetail(DrizzleType type, int cnt) {
+      this.drizzleType = type;
+      this.cnt = cnt;
     }
-    @Embeddable
-    @Getter
-    @NoArgsConstructor
-    public static class EspressoDetail{
-        @Enumerated(EnumType.STRING)
-        private EspressoType espressoType;
+  }
 
-        private Integer cnt;
+  @Embeddable
+  @Getter
+  @NoArgsConstructor
+  public static class EspressoDetail {
+    @Enumerated(EnumType.STRING)
+    private EspressoType espressoType;
 
-        public EspressoDetail(EspressoType type, int cnt){
-            this.espressoType = type;
-            this.cnt = cnt;
-        }
+    private Integer cnt;
+
+    public EspressoDetail(EspressoType type, int cnt) {
+      this.espressoType = type;
+      this.cnt = cnt;
     }
-    @Embeddable
-    @Getter
-    @NoArgsConstructor
-    public static class MilkDetail{
-        @Enumerated(EnumType.STRING)
-        private MilkType milkType;
+  }
 
-        @Enumerated(EnumType.STRING)
-        private Amount milkAmount;
+  @Embeddable
+  @Getter
+  @NoArgsConstructor
+  public static class FoamDetail {
+    @Enumerated(EnumType.STRING)
+    private FoamType foamType;
 
-        public MilkDetail(MilkType type, Amount amount){
-            this.milkType = type;
-            this.milkAmount = amount;
-        }
+    @Enumerated(EnumType.STRING)
+    private Amount foamAmount;
+
+    public FoamDetail(FoamType type, Amount amount) {
+      this.foamType = type;
+      this.foamAmount = amount;
     }
-    @Embeddable
-    @Getter
-    @NoArgsConstructor
-    public static class FoamDetail{
-        @Enumerated(EnumType.STRING)
-        private FoamType foamType;
+  }
 
-        @Enumerated(EnumType.STRING)
-        private Amount foamAmount;
+  @Embeddable
+  @Getter
+  @NoArgsConstructor
+  public static class PotionDetail {
+    @Enumerated(EnumType.STRING)
+    private PotionType potionType;
 
-        public FoamDetail(FoamType type, Amount amount){
-            this.foamType = type;
-            this.foamAmount = amount;
-        }
+    private Integer cnt;
+
+    public PotionDetail(PotionType type, int cnt) {
+      this.potionType = type;
+      this.cnt = cnt;
     }
+  }
 
-    @Embeddable
-    @Getter
-    @NoArgsConstructor
-    public static class PotionDetail{
-        @Enumerated(EnumType.STRING)
-        private PotionType potionType;
-        private Integer cnt;
+  @Getter
+  @NoArgsConstructor
+  public enum PotionType {
+    BUTTER(500),
+    CREAM_CHEESE(1000);
 
-        public PotionDetail(PotionType type, int cnt){
-            this.potionType = type;
-            this.cnt = cnt;
-        }
+    private Integer price;
+
+    PotionType(Integer price) {
+      this.price = price;
     }
-    @Getter
-    @NoArgsConstructor
-    public enum PotionType{
-        BUTTER(500),
-        CREAM_CHEESE(1000);
-
-        private Integer price;
-        PotionType(Integer price) {
-            this.price = price;
-        }
-    }
+  }
 }
--- src/main/java/com/devlop/siren/domain/item/entity/option/OptionTypeGroup.java
+++ src/main/java/com/devlop/siren/domain/item/entity/option/OptionTypeGroup.java
@@ -1,26 +1,84 @@
 package com.devlop.siren.domain.item.entity.option;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public class OptionTypeGroup {
 
-    public enum Temperature{
-        HOT, COLD, NONE
+  public enum Temperature {
+    HOT,
+    COLD,
+    NONE;
+
+    @JsonCreator
+    public Temperature fromString(String value) {
+      return Temperature.valueOf(value.toUpperCase());
     }
-    public enum Amount{
-        LITTLE, NORMAL, MUCH, NONE;
+  }
+
+  public enum Amount {
+    LITTLE,
+    NORMAL,
+    MUCH,
+    NONE;
+
+    @JsonCreator
+    public Amount fromString(String value) {
+      return Amount.valueOf(value.toUpperCase());
     }
-    public enum EspressoType{
-        ORIGINAL, DECAFFEINE
+  }
+
+  public enum EspressoType {
+    ORIGINAL,
+    DECAFFEINE
+    @JsonCreator
+    public EspressoType fromString(String value) {
+      return EspressoType.valueOf(value.toUpperCase());
     }
-    public enum MilkType{
-        ORIGINAL, LOW_FAT, FAT_FREE, SOY, OAT
+  }
+
+  public enum MilkType {
+    ORIGINAL,
+    LOW_FAT,
+    FAT_FREE,
+    SOY,
+    OAT;
+
+    @JsonCreator
+    public MilkType fromString(String value) {
+      return MilkType.valueOf(value.toUpperCase());
     }
-    public enum FoamType{
-        MILK, ESPRESSO, MATCHA
+  }
+
+  public enum FoamType {
+    MILK,
+    ESPRESSO,
+    MATCHA;
+
+    @JsonCreator
+    public FoamType fromString(String value) {
+      return FoamType.valueOf(value.toUpperCase());
     }
-    public enum SyrupType{
-        VANILLA, CARAMEL, HAZELNUT, CLASSIC;
+  }
+
+  public enum SyrupType {
+    VANILLA,
+    CARAMEL,
+    HAZELNUT,
+    CLASSIC;
+
+    @JsonCreator
+    public SyrupType fromString(String value) {
+      return SyrupType.valueOf(value.toUpperCase());
     }
-    public enum DrizzleType{
-        CHOCOLATE, CARAMEL;
+  }
+
+  public enum DrizzleType {
+    CHOCOLATE,
+    CARAMEL;
+
+    @JsonCreator
+    public DrizzleType fromString(String value) {
+      return DrizzleType.valueOf(value.toUpperCase());
     }
+  }
 }
