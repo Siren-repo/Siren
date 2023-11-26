304ecbca1cde1cdbbc7068289751f24c328a82dc
yooyouny <jhjin2u@hanmail.com>
2023-11-27 01:19:51 +0900

    feat: 주문 엔티티 수정 (#23) - totalOrderAmount, totalQuantity 변수 추가 - Amount 계산을 OrderItem에서 생성

--- src/main/java/com/devlop/siren/domain/order/domain/OrderItem.java
+++ src/main/java/com/devlop/siren/domain/order/domain/OrderItem.java
@@ -2,55 +2,65 @@ package com.devlop.siren.domain.order.domain;
 
 import com.devlop.siren.domain.item.entity.Item;
 import com.devlop.siren.domain.order.domain.option.CustomOption;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import javax.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "order_items", uniqueConstraints = {
-        @UniqueConstraint(columnNames = {"order_id", "item_id"})
-})
+@Table(
+    name = "order_items",
+    uniqueConstraints = {@UniqueConstraint(columnNames = {"order_id", "item_id"})})
 public class OrderItem {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "order_id", nullable = false)
-    private Order order;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "order_id", nullable = false)
+  @JsonBackReference
+  private Order order;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "item_id")
-    private Item item;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "item_id")
+  private Item item;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "custom_option_id")
-    private CustomOption customOption;
+  @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+  @JoinColumn(name = "custom_option_id")
+  private CustomOption customOption;
 
-    private Integer quantity;
+  private Integer quantity;
+  private Integer amount;
 
-    public static OrderItem create(Item item, CustomOption customOption, Integer quantity){
-        OrderItem orderItem = new OrderItem();
-        orderItem.setItem(item);
-        orderItem.setCustomOption(customOption);
-        orderItem.setQuantity(quantity);
-        //TODO :: item.deduct(quantity)
-        return orderItem;
-    }
-    public void setOrder(Order order){
-        this.order = order;
-    }
-    private void setItem(Item item){
-        this.item = item;
-    }
-    private void setCustomOption(CustomOption option){
-        this.customOption = option;
-    }
-    private void setQuantity(Integer quantity){
-        this.quantity = quantity;
-    }
+  public static OrderItem create(Item item, CustomOption customOption, Integer quantity) {
+    OrderItem orderItem = new OrderItem();
+    orderItem.setItem(item);
+    orderItem.setCustomOption(customOption);
+    orderItem.setQuantity(quantity);
+    orderItem.setAmount((item.getPrice() + customOption.getAdditionalAmount()) * quantity);
+    return orderItem;
+  }
+
+  public void setOrder(Order order) {
+    this.order = order;
+  }
+
+  private void setItem(Item item) {
+    this.item = item;
+  }
+
+  private void setCustomOption(CustomOption option) {
+    this.customOption = option;
+  }
+
+  private void setQuantity(Integer quantity) {
+    this.quantity = quantity;
+  }
+
+  private void setAmount(Integer amount) {
+    this.amount = amount;
+  }
 }
