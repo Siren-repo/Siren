75808496087c1c41a396bac25fc4303d695b93ec
yooyouny <jhjin2u@hanmail.com>
2023-11-27 01:28:09 +0900

    feat: CustomOption 엔티티 수정 (#23) - AdditionalAmount를 속성에 맞게 변경 - null일 수도 있으므로 객체 null 허용 처리

--- src/main/java/com/devlop/siren/domain/order/domain/option/CustomOption.java
+++ src/main/java/com/devlop/siren/domain/order/domain/option/CustomOption.java
@@ -1,29 +1,57 @@
 package com.devlop.siren.domain.order.domain.option;
 
-import com.devlop.siren.domain.item.entity.option.OptionTypeGroup.*;
-
-import javax.persistence.*;
+import com.devlop.siren.domain.item.entity.option.OptionTypeGroup.Temperature;
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
 @DiscriminatorColumn
 public abstract class CustomOption {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "custom_option_id")
-    private Long id;
+  static Set<String> categoriesWithDefaultOptions =
+      Set.of("Cake", "Salad", "IceCream", "Fruit", "Yogurt", "Snack");
+  static Set<String> categoriesWithPotionOption = Set.of("Bread");
+  static Set<String> categoriesRequiringWarming = Set.of("Sandwich", "Bread", "Soup");
 
-    @Column(name = "take_out", nullable = false)
-    protected Boolean takeout;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "custom_option_id")
+  private Long id;
 
-    @Column(name = "temperature", nullable = false)
-    @Enumerated(EnumType.STRING)
-    protected Temperature temperature;
+  @Column(name = "take_out", nullable = false)
+  protected Boolean takeout;
 
-    @Column(name = "additional_amount")
-    protected int amount = 0;
+  @Column(name = "temperature", nullable = false)
+  @Enumerated(EnumType.STRING)
+  protected Temperature temperature;
 
-    public abstract int getAdditionalAmount();
+  @Column(name = "additional_amount")
+  protected int amount = 0;
+
+  public Boolean getTakeout() {
+    return takeout;
+  }
+
+  public void setTakeout(Boolean takeout) {
+    this.takeout = takeout;
+  }
+
+  public Temperature getTemperature() {
+    return temperature;
+  }
+
+  public void setTemperature(Boolean warm) {
+    this.temperature = warm ? Temperature.HOT : Temperature.COLD;
+  }
+
+  public abstract Integer getAdditionalAmount();
 }
-
-
--- src/main/java/com/devlop/siren/domain/order/domain/option/FoodOption.java
+++ src/main/java/com/devlop/siren/domain/order/domain/option/FoodOption.java
@@ -1,61 +1,68 @@
 package com.devlop.siren.domain.order.domain.option;
 
 import com.devlop.siren.domain.item.entity.Item;
-import com.devlop.siren.domain.item.entity.option.OptionDetails.*;
-import com.devlop.siren.domain.item.entity.option.OptionTypeGroup.*;
-
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
+import com.devlop.siren.domain.item.entity.option.OptionDetails.PotionDetail;
+import com.devlop.siren.domain.item.entity.option.OptionTypeGroup.Temperature;
+import com.devlop.siren.domain.order.dto.request.CustomOptionRequest;
 import java.util.HashSet;
 import java.util.Set;
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Entity
 @DiscriminatorValue("Food")
 @NoArgsConstructor
 @Table(name = "food_options")
-public class FoodOption extends CustomOption{
-    /*
-    * 추가 옵션 없는 카테고리: 케이크, 샐러드, 아이스크림, 과일, 요거트, 스낵
-    * 포션 옵션 추가되는 카테고리: 브레드
-    * 데움 옵션 추가되는 카테고리: 샌드위치, 브레드, 수프
-    * */
-    @ElementCollection
-    @CollectionTable(name = "potion_details",
-            joinColumns = @JoinColumn(name="custom_option_id"))
-    @Column(name = "potion_type")
-    private Set<PotionDetail> potion = new HashSet<PotionDetail>();
+public class FoodOption extends CustomOption {
+  @ElementCollection
+  @CollectionTable(name = "potion_details", joinColumns = @JoinColumn(name = "custom_option_id"))
+  @Column(name = "potion_type")
+  private Set<PotionDetail> potion = new HashSet<PotionDetail>();
 
-    static Set<String> categoriesWithoutOptions = Set.of("Cake", "Salad", "IceCream", "Fruit", "Yogurt", "Snack");
-    static Set<String> categoriesWithPotionOption = Set.of("Bread");
-    static Set<String> categoriesRequiringWarming = Set.of("Sandwich", "Bread", "Soup");
+  @Builder
+  public FoodOption(Item item, Boolean isWarmed, Boolean isTakekout, Set<PotionDetail> potions) {
+    takeout = isTakekout;
 
-    @Builder
-    public FoodOption(Item item, Boolean isWarmed, Boolean isTakeOut, Set<PotionDetail> potions) {
-        takeout = isTakeOut;
-
-        String categoryName = item.getCategory().getCategoryName();
-        if(categoriesRequiringWarming.contains(categoryName))
-            temperature = isWarmed ? Temperature.HOT : Temperature.NONE;
-
-        if(categoriesWithoutOptions.contains(categoryName)){
-            temperature = Temperature.NONE;
-        }
-
-        if(categoriesWithPotionOption.contains(categoryName)){
-            potion = potions;
-        }
-    }
-    @Override
-    public int getAdditionalAmount() {
-        if(potion.size() > 0){
-            amount += potion.stream()
-                    .mapToInt(potion -> potion.getPotionType().getPrice() * potion.getCnt())
-                    .sum();
-        }
-        return amount;
+    String categoryName = item.getCategory().getCategoryName();
+    if (categoriesRequiringWarming.contains(categoryName)) {
+      temperature = isWarmed ? Temperature.HOT : Temperature.NONE;
     }
 
+    if (categoriesWithDefaultOptions.contains(categoryName)) {
+      temperature = Temperature.NONE;
+    }
 
+    if (categoriesWithPotionOption.contains(categoryName)) {
+      potion = potions;
+    }
+
+    amount = getAdditionalAmount();
+  }
+
+  public static FoodOption fromDto(CustomOptionRequest request, Boolean takeout, Boolean warm) {
+    return FoodOption.builder()
+        .isWarmed(warm)
+        .isTakekout(takeout)
+        .potions(request.getPotions().orElse(new HashSet<>()))
+        .build();
+  }
+
+  @Override
+  public Integer getAdditionalAmount() {
+    int additionalAmount = 0;
+    if (potion.size() > 0) {
+      additionalAmount +=
+          potion.stream()
+              .mapToInt(potion -> potion.getPotionType().getPrice() * potion.getCnt())
+              .sum();
+    }
+    return additionalAmount;
+  }
 }
