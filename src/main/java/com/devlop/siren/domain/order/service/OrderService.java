c3a2f4d656ee84c7fe455746a1f637e557900f55
yooyouny <jhjin2u@hanmail.com>
2023-11-28 00:10:43 +0900

    feat: 주문 생성으로 인한 재고감소 처리 (#23) - Order 엔티티를 repotiroy에 저장하기 전 OrderItem의 quantity만큼 찾아온 stock을 감소시킴

--- src/main/java/com/devlop/siren/domain/order/service/OrderService.java
+++ src/main/java/com/devlop/siren/domain/order/service/OrderService.java
@@ -2,18 +2,16 @@ package com.devlop.siren.domain.order.service;
 
 import com.devlop.siren.domain.order.domain.Order;
 import com.devlop.siren.domain.order.domain.OrderItem;
-import com.devlop.siren.domain.order.domain.OrderStatus;
 import com.devlop.siren.domain.order.domain.option.CustomOption;
-import com.devlop.siren.domain.order.dto.response.OrderCreateResponse;
+import com.devlop.siren.domain.order.dto.response.OrderDetailResponse;
 import com.devlop.siren.domain.order.repository.CustomOptionRepository;
 import com.devlop.siren.domain.order.repository.OrderItemRepository;
 import com.devlop.siren.domain.order.repository.OrderRepository;
+import com.devlop.siren.domain.stock.repository.StockRepository;
 import com.devlop.siren.domain.store.domain.Store;
 import com.devlop.siren.domain.user.domain.User;
-import com.devlop.siren.domain.user.dto.UserDetailsDto;
 import com.devlop.siren.global.common.response.ResponseCode;
 import com.devlop.siren.global.exception.GlobalException;
-import com.devlop.siren.global.util.UserInformation;
 import java.time.LocalTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +24,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class OrderService {
   private final OrderRepository orderRepository;
   private final OrderItemRepository orderItemRepository;
+  private final StockRepository stockRepository;
   private final CustomOptionRepository customOptionRepository;
 
   @Transactional
@@ -34,37 +33,28 @@ public class OrderService {
   }
 
   @Transactional
-  public OrderCreateResponse create(User user, Store store, List<OrderItem> orderItems) {
+  public OrderDetailResponse create(User user, Store store, List<OrderItem> orderItems) {
     isStoreOperating(store, LocalTime.now());
     Order newOrder = Order.of(user, store, orderItems);
     orderItems.forEach(
         orderItem -> {
+          consumeStock(
+              orderItem.getQuantity(), store.getStoreId(), orderItem.getItem().getItemId());
           orderItem.setOrder(newOrder);
           orderItemRepository.save(orderItem);
         });
-    return OrderCreateResponse.of(orderRepository.save(newOrder));
+    return OrderDetailResponse.of(orderRepository.save(newOrder));
+  }
+
+  public void consumeStock(Integer quantity, Long storeId, Long itemId) {
+    stockRepository
+        .findByStoreAndItem(storeId, itemId)
+        .ifPresent(stock -> stock.consumed(quantity));
   }
 
   public void isStoreOperating(Store store, LocalTime now) {
     if (now.isBefore(store.getOpenTime()) || now.isAfter(store.getCloseTime())) {
       throw new GlobalException(ResponseCode.ErrorCode.NOT_OPERATING_TIME);
     }
-  }
-
-  @Transactional
-  public List<OrderItem> cancel(Long orderId, UserDetailsDto userDto) {
-    UserInformation.validStaff(userDto);
-
-    Order order =
-        orderRepository
-            .findById(orderId)
-            .orElseThrow(() -> new GlobalException(ResponseCode.ErrorCode.NOT_FOUND_ORDER));
-
-    if (!OrderStatus.INIT.equals(order.getStatus())) {
-      throw new GlobalException(ResponseCode.ErrorCode.NOT_CANCEL_ORDER);
-    }
-
-    order.cancel();
-    return order.getOrderItems();
   }
 }
