0eef160d6d564cb969b83648465b61d3c863e636
yoonjuho <dbswn990@naver.com>
2023-12-11 03:05:51 +0900

    refactor: Category Controller 권한 검증 추가 (#44)

--- src/main/java/com/devlop/siren/domain/category/controller/CategoryController.java
+++ src/main/java/com/devlop/siren/domain/category/controller/CategoryController.java
@@ -4,11 +4,14 @@ import com.devlop.siren.domain.category.dto.request.CategoryCreateRequest;
 import com.devlop.siren.domain.category.dto.response.CategoryResponse;
 import com.devlop.siren.domain.category.entity.CategoryType;
 import com.devlop.siren.domain.category.service.CategoryService;
+import com.devlop.siren.domain.user.domain.UserRole;
 import com.devlop.siren.domain.user.dto.UserDetailsDto;
 import com.devlop.siren.global.common.response.ApiResponse;
 import com.devlop.siren.global.common.response.ResponseCode;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+
+import com.devlop.siren.global.util.Permission;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -26,12 +29,12 @@ public class CategoryController {
 
   private final CategoryService categoryService;
 
+  @Permission(role = {UserRole.ADMIN})
   @PostMapping
   public ApiResponse<CategoryResponse> createCategory(
-      @RequestBody @Valid CategoryCreateRequest categoryCreateRequest,
-      @AuthenticationPrincipal UserDetailsDto user) {
+      @RequestBody @Valid CategoryCreateRequest categoryCreateRequest) {
     return ApiResponse.ok(
-        ResponseCode.Normal.CREATE, categoryService.register(categoryCreateRequest, user));
+        ResponseCode.Normal.CREATE, categoryService.register(categoryCreateRequest));
   }
 
   @GetMapping
