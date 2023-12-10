7ab269a36870f382a726ac855fbcc26902e02cdb
yoonjuho <dbswn990@naver.com>
2023-12-11 03:04:44 +0900

    refactor: Stock Controller 권한 검증 추가 (#44)

--- src/main/java/com/devlop/siren/domain/stock/controller/StockController.java
+++ src/main/java/com/devlop/siren/domain/stock/controller/StockController.java
@@ -3,9 +3,11 @@ package com.devlop.siren.domain.stock.controller;
 import com.devlop.siren.domain.stock.dto.request.StockCreateRequest;
 import com.devlop.siren.domain.stock.dto.response.StockResponse;
 import com.devlop.siren.domain.stock.service.StockService;
+import com.devlop.siren.domain.user.domain.UserRole;
 import com.devlop.siren.domain.user.dto.UserDetailsDto;
 import com.devlop.siren.global.common.response.ApiResponse;
 import com.devlop.siren.global.common.response.ResponseCode;
+import com.devlop.siren.global.util.Permission;
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
@@ -34,52 +36,50 @@ public class StockController {
   private final StockService stockService;
 
   // 재고를 생성
+  @Permission(role = {UserRole.ADMIN, UserRole.STAFF})
   @PostMapping
-  public ApiResponse<StockResponse> createStock(
-      @RequestBody @Valid StockCreateRequest stockCreateRequest,
-      @AuthenticationPrincipal UserDetailsDto user) {
-    return ApiResponse.ok(
-        ResponseCode.Normal.CREATE, stockService.create(stockCreateRequest, user));
+  public ApiResponse<StockResponse> create(
+      @RequestBody @Valid StockCreateRequest stockCreateRequest) {
+    return ApiResponse.ok(ResponseCode.Normal.CREATE, stockService.create(stockCreateRequest));
   }
 
   // 매장별 모든 재고를 조회
+  @Permission(role = {UserRole.ADMIN, UserRole.STAFF})
   @GetMapping(value = "/{storeId}")
   public ApiResponse<Page<StockResponse>> findAllByStore(
       @PathVariable @Min(1L) Long storeId,
-      @AuthenticationPrincipal UserDetailsDto user,
       @PageableDefault(sort = "stockId", direction = Sort.Direction.DESC) Pageable pageable) {
     return ApiResponse.ok(
-        ResponseCode.Normal.RETRIEVE, stockService.findAllByStore(storeId, user, pageable));
+        ResponseCode.Normal.RETRIEVE, stockService.findAllByStore(storeId, pageable));
   }
 
   // 매장별 특정 상품 재고를 조회
+  @Permission(role = {UserRole.ADMIN, UserRole.STAFF})
   @GetMapping(value = "/{storeId}/{itemId}")
   public ApiResponse<StockResponse> findStockDetail(
-      @PathVariable @Min(1L) Long storeId,
-      @PathVariable @Min(1L) Long itemId,
-      @AuthenticationPrincipal UserDetailsDto user) {
+      @PathVariable @Min(1L) Long storeId, @PathVariable @Min(1L) Long itemId) {
     return ApiResponse.ok(
-        ResponseCode.Normal.RETRIEVE, stockService.findByStoreAndItem(storeId, itemId, user));
+        ResponseCode.Normal.RETRIEVE, stockService.findByStoreAndItem(storeId, itemId));
   }
 
   // 재고를 수정
+  @Permission(role = {UserRole.ADMIN, UserRole.STAFF})
   @PutMapping(value = "/{storeId}/{itemId}")
-  public ApiResponse<StockResponse> updateStock(
+  public ApiResponse<StockResponse> update(
       @PathVariable @Min(1L) Long storeId,
       @PathVariable @Min(1L) Long itemId,
-      @RequestParam @NotNull @Min(0) Integer stock,
-      @AuthenticationPrincipal UserDetailsDto user) {
+      @RequestParam @NotNull @Min(0) Integer stock) {
     return ApiResponse.ok(
-        ResponseCode.Normal.UPDATE, stockService.updateStock(storeId, itemId, stock, user));
+        ResponseCode.Normal.UPDATE, stockService.update(storeId, itemId, stock));
   }
 
   // 재고를 삭제
+  @Permission(role = {UserRole.ADMIN, UserRole.STAFF})
   @DeleteMapping(value = "/{storeId}/{itemId}")
-  public ApiResponse<?> deleteStock(
+  public ApiResponse<?> delete(
       @PathVariable @Min(1L) Long storeId,
-      @PathVariable @Min(1L) Long itemId,
-      @AuthenticationPrincipal UserDetailsDto user) {
-    stockService.deleteStock(storeId, itemId, user);
+      @PathVariable @Min(1L) Long itemId) {
+    stockService.delete(storeId, itemId);
     return ApiResponse.ok(
         ResponseCode.Normal.DELETE, String.format("StoreId = %d ItemId = %d", storeId, itemId));
   }
