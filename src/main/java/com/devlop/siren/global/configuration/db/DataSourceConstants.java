e48f1ded344db62d0dbda77c71d272a521f614fb
yoonjuho <dbswn990@naver.com>
2023-12-27 17:57:10 +0900

    feat: replication 구조 상수화 (#48)

---
+++ src/main/java/com/devlop/siren/global/configuration/db/DataSourceConstants.java
@@ -0,0 +1,6 @@
+package com.devlop.siren.global.configuration.db;
+
+public class DataSourceConstants {
+    public static final String MASTER = "master";
+    public static final String SLAVE = "slave";
+}
